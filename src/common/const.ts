16c16a6aa0083c898e2aaed45bd9b6a6c6b1d9a6
horihiro <hori.kawa.shima.tool+github@gmail.com>
2024-03-26 16:03:53 +0900

    Fix RegExp for the domain including numbers

--- src/common/const.ts
+++ src/common/const.ts
@@ -2,4 +2,4 @@ export const DOMAINS_HTTP_GET = ['t.co'];
 export const SELECTOR_CARD_TIMELINE = '*[data-testid="card.wrapper"]:not([data-validated])';
 export const SELECTOR_CARD_EMBEDDED = ':has(>[data-testid="tweetText"]) + div > div:not([data-validated])';
 export const CONNECTION_NAME = 'validateUrl';
-export const REGEXP_DOMAIN = /^(?:[a-zA-Z\\d\\-]+\.)+[a-zA-Z]+$/;
+export const REGEXP_DOMAIN = /^(?:[a-zA-Z\d\-]+\.)+[a-zA-Z]+$/;
